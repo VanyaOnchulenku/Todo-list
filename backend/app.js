024d17709c06db448e4a769364979467d61f1155
Vanya <ioan.onchulenku@gmail.com>
2023-05-13 19:54:24 +0200

    API request - created

--- backend/app.js
+++ backend/app.js
@@ -9,7 +9,51 @@ app.use(cors())
 
 const conn = mysql.createConnection({
     host: 'localhost',
-    user: 'ivan',
+    user: 'root',
     password: 'root',
-    database: 'todo'
+    database: 'schema'
+})
+
+
+app.get('/animals', (req, res) => {
+    const q = 'SELECT * FROM animals'
+    conn.query(q,(err, data) =>{
+        if(err) res.send(err)
+        else res.send(data)
+    })  
+})
+
+
+app.post('/animals', (req,res)=> {
+    const animal = req.body
+    const q = 'INSERT INTO animals (`type`, `breed`, `desc`, `age`, `host`) VALUES (?)'
+    const values = [animal.type, animal.breed, animal.desc, animal.age, animal.host]
+    conn.query(q, [values], (err, data) => { 
+        if(err) res.send(err)
+        else res.send("Your pet successfully added")
+    })
+})
+
+app.delete('/animals/:id', (req, res) => {
+    const q = 'DELETE FROM animals WHERE id = ?'
+    conn.query(q,[req.params.id], (err, data) => {
+        if(err) res.send(err)
+        else res.send('Your pet successfully removed from the list')
+    })
+})
+
+app.put('/animals/:id', (req,res) => {
+    const animalId = req.params.id
+    const animal = req.body
+    const q = 'UPDATE animals SET `type` = ?, `breed` = ?, `desc` = ?, `age` = ?, `host` = ? WHERE `id` = ?'
+    const values = [animal.type, animal.breed, animal.desc, animal.age, animal.host]
+    conn.query(q,[...values, animalId], (err,data) => {
+        if (err) res.send(err)
+        else res.send('You successfully updated information about your pet')
+    })
+})
+
+
+app.listen(3000,()=>{
+    console.log('Animals in da hus')
 })
